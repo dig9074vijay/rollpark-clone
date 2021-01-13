8aa54dfeea654be953852d443e96c624b7020333
Digvijay Srivastava <dig9074vijay@gmail.com>
2021-01-13 19:27:19 +0530

    Fixed warnings

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import Header from "./HeaderComponent";
-import Footer from "./FooterComponent";
-import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+
+import { Switch,  withRouter } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Main extends Component {
