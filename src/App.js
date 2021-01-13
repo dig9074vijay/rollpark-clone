fb817a9b44e90cf6eb38ac2317486d971de9d2d4
Digvijay Srivastava <dig9074vijay@gmail.com>
2021-01-13 16:50:22 +0530

    Added MainComponent to render all other components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Header from "./components/HeaderComponent";
+import Main from "./components/MainComponent";
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
-    </div>
+    
+      <BrowserRouter>
+        <div className="App">
+          <Main/>
+        </div>
+      </BrowserRouter>
+     
   );
 }
 
---
+++ src/components/MainComponent.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+class Main extends Component {
+  render() {
+  
+    return (
+      <div>
+        <Header />
+        <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch location={this.props.location}>
+                  
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        
+      </div>
+    );
+  }
+}
+
+export default withRouter(Main);
