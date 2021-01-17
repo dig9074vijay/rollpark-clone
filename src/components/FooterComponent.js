75db0b4494ddbbe62e41c70e35f43c21d2844907
Digvijay Srivastava <dig9074vijay@gmail.com>
2021-01-17 22:54:40 +0530

    Added Footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,32 +1,21 @@
+
+
 import React from "react";
+import {Jumbotron} from "reactstrap";
 
-
-const Jumbotron = () => {
-    <div>
-        <Jumbotron>
-          <div className="container">
-            <div className="row row-header">
-              <div className="col-12 col-sm-4">
-                <h1>Designed for you</h1>
-                </div>  
-                <div className="col-12 col-sm-4">
-                  <br/>
-                  <ul className="list-unstyled">
-                    <li>-Roll it outand instantly park</li>
-                    <li>-Re-use it on multiple sites</li>
-                    <li>-Event parking, Overflow, Vehicle Storage</li>
-                    <li>-Daily temporary use <i style={{color:"green"}}>1-3 years</i></li>
-                    <li>-Daily permanent use <i style={{color:"green"}}>1-3 years</i></li>
-                  </ul>
-                </div>
-                <div className="col-12 col-sm-4">
-                  <img className="shadow" src="./rollpark.png" alt="Rollpark" width="300" height="300"/>
-                </div>
-              
-            </div>
-          </div>
-        </Jumbotron>
-    </div>
+const Row6 = () => {
+    return(
+        <div>
+            <br/><br/>
+      <hr/>
+      <Jumbotron className="jumbobg2">
+        <div></div>
+      </Jumbotron>
+      <hr/>
+      </div>
+      );
+        
+    
 }
 
-export default Jumbotron;
+export default Row6 ;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Row2 from "./Row2";
 import CardRow3 from "./Cards";
 import Row4 from "./Row4";
 import Row5 from "./Row5";
+import Row6 from "./FooterComponent";
 
 
 class Main extends Component {
@@ -18,6 +19,7 @@ class Main extends Component {
         <CardRow3/>
         <Row4/>
         <Row5/>
+        <Row6 />
       </div>
     );
   }
